33e493759be83c1545b4e6168a09ee17548e4a48
Margh123 <68310055+Margh123@users.noreply.github.com>
2022-05-14 12:26:15 +0700

    GDPR

---
+++ New folder/CookieConsent.js
@@ -0,0 +1,52 @@
+
+// put default value for a cookie whenever it is created
+var cookieKey= "Accepted";
+var cookieValue="true";
+document.cookie = "abc=false"; // for testing purpose
+function myGreeting() {
+  document.getElementById("popup").style.display = "none"
+}
+// "i understand" button in action
+let accept= document.getElementById("accept");
+accept.onclick= function(){
+    setCookie();
+}
+// cookie setter
+ let setCookie= function(){
+  document.cookie = "Accepted=true";
+  if(document.cookie){ // Check if cookies are allowed
+    document.getElementById("popup").innerHTML = "Your related cookies have been stored. ";
+	const myTimeout = setTimeout(myGreeting, 3000);
+  }else{
+    alert("Set cookie is unsuccessful.Please enable cookies in your browser");
+  }
+ }
+// cookie getter
+let getCookie= function(){ 
+  let nameGet = "Accepted=true";
+  let tokens = document.cookie.split(';');
+  for(let i = 0; i < tokens.length; i++) { //[1]
+    let c = tokens[i];
+    while (c.charAt(0) == ' ') { //This code is to truncate all beginning spaces of cookies yielded in an array tokens.
+      c = c.substring(1);
+    }
+    if (c.indexOf(nameGet) == 0) {
+      return "true";
+    }
+  }
+  return "";
+}
+// check cookie status
+let isSetCookie= function(){
+    let value=getCookie();
+    if(value!=""){
+		alert("welcome back!");
+        document.getElementById("popup").style.display = "none";
+    }else{
+        
+        document.getElementById("popup").style.display = "block";
+    }
+}
+isSetCookie();
+
+//[1] W3schools.com. 2018.JavaScript Cookies. [online] Available at: <https://www.w3schools.com/js/js_cookies.asp> [Accessed 12 May 2022].
