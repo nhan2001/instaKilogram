e1bbbb2bb66605d42ed7f35b5412c841c3f0dfee
SonCaos3916151 <86813958+SonCaos3916151@users.noreply.github.com>
2022-05-17 21:09:07 +0700

    Update editProfile.php

--- Son's work/editProfile.php
+++ Son's work/editProfile.php
@@ -1,12 +1,46 @@
 <?php
-    require('actions.php');
-?>
-<?php
+    //$UserEmail = "";
+    if(isset($UserEmail['userEmail']) ? $UserEmail['userEmail']: 'Default' ){
+        $UserEmail = $_GET['userEmail'];
+    }
+    $UserFname  = $_GET['userFname'];
+    $UserLname = $_GET['userLname'];
+    $UserName = $_GET['userName'];
+
+    $images = glob("profileImages/"."*.{jpeg,jpg,gif,png}",GLOB_BRACE);
+    foreach($images as $image){
+
+        list($imageName) = explode('.',$image); //return a string without the file extension==>private/1_son.png  = private/1_son
+        if($imageName == "profileImages/".$UserName){ //if name of file is equal to current username, they can view the image in this private display section
+            $imageName = substr($image, strpos($image, "/") + 1); //return a string that comes after the underscore(_);
+            
+            $currentImage = $imageName; //current user image, this line is necessary
+        }
+    }
+
+
+    if (isset($_POST['submitBtn'])){
+        $newPassword = isset($_POST['password']) ? $_POST['password'] : '';
+        echo $newPassword;
+    }
+
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>Page Title</title>
+        <style>
+            /*For image, fix this however you like to center it*/
+            .imgcontainer {
+            text-align: center;
+            margin: 24px 0 12px 0;
+            position: relative;
+            }
+            img.avatar {
+            width: 40%;
+            border-radius: 50%;
+            }
+        </style>
     </head>
 <body>
     <header>
@@ -21,16 +55,17 @@
         </nav>
     </header>
     <h1>Edit User Profile page</h1>
-    <p>Below is the user information</p>
+    <p>Below is this user information</p>
 
-    <?php
-        $UserEmail = $_GET['userEmail'];
-
-        $UserFname  = $_GET['userFname'];
-        $UserLname = $_GET['userLname'];
-        $UserName = $_GET['userName'];
-    ?>
-    
+    <div class = "imagecontainer">
+            <img src ="<?php
+                echo "profileImages/$currentImage";
+            ?>
+            " alt="Avatar" class="avatar">
+    </div>
+        <?php
+            echo $currentImage;
+        ?>
     <div id=displayUserInfo>
         <table style="width:100%">
             <tr>
@@ -51,7 +86,12 @@
             </tr>
             <tr>
                 <th>Password:</th>
-                <td><?php ?></td>
+                <td>
+                    <form action ="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="changePasswordform">
+                        <input type="password" name = "password"placeholder="Enter new password">
+                        <input type="submit" name="submitBtn" id = "submit" value="Submit">
+                    </form>
+                </td>
             </tr>
         </table>
     </div>
