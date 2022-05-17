3eada489807f98c59f130bd4844d844170d96f98
SonCaos3916151 <86813958+SonCaos3916151@users.noreply.github.com>
2022-05-18 00:22:54 +0700

    Update editProfile.php

--- Group 9/editProfile.php
+++ Group 9/editProfile.php
@@ -1,27 +1,50 @@
 <?php
-    //$UserEmail = "";
-    if(isset($UserEmail['userEmail']) ? $UserEmail['userEmail']: 'Default' ){
+    if(isset($_GET['userEmail'])){
         $UserEmail = $_GET['userEmail'];
     }
-    $UserFname  = $_GET['userFname'];
-    $UserLname = $_GET['userLname'];
-    $UserName = $_GET['userName'];
-
-    $images = glob("profileImages/"."*.{jpeg,jpg,gif,png}",GLOB_BRACE);
-    foreach($images as $image){
-
-        list($imageName) = explode('.',$image); //return a string without the file extension==>private/1_son.png  = private/1_son
-        if($imageName == "profileImages/".$UserName){ //if name of file is equal to current username, they can view the image in this private display section
-            $imageName = substr($image, strpos($image, "/") + 1); //return a string that comes after the underscore(_);
-            
-            $currentImage = $imageName; //current user image, this line is necessary
+    if(isset($_GET['userName'])){
+        $userName = $_GET['userName'];
+        $images = glob("profileImages/"."*.{jpeg,jpg,gif,png}",GLOB_BRACE);
+        foreach($images as $image){
+    
+            list($imageName) = explode('.',$image); //return a string without the file extension==>private/1_son.png  = private/1_son
+            if($imageName == "profileImages/".$userName){ //if name of file is equal to current username, they can view the image in this private display section
+                $imageName = substr($image, strpos($image, "/") + 1); //return a string that comes after the underscore(_);
+                
+                $currentImage = $imageName; //current user image, this line is necessary
+            }
         }
     }
 
+    if (isset($_GET['submitBtn'])){
+        $newPassword = htmlentities($_GET['password']);
+        $pfile = fopen('accounts.db','r+');
+        rewind($pfile);
 
-    if (isset($_POST['submitBtn'])){
-        $newPassword = isset($_POST['password']) ? $_POST['password'] : '';
-        echo $newPassword;
+        while (!feof($pfile)) {
+                $line = fgets($pfile);
+                $tmp = explode(',', $line);
+                if(str_contains($tmp[0],"gmail.com" )){ //not an empty line
+                    if($tmp[0] == $UserEmail){
+                        $hashedPass = password_hash($newPassword,PASSWORD_DEFAULT);
+                        $tmp[4] = $hashedPass;
+                        $replacedLine = $tmp[0] + "," + $tmp[1] + "," + $tmp[2] + "," + $tmp[3] + "," + $tmp[4];
+                        
+
+                        $file_name = "accounts.db";
+            
+                        function file_edit_contents($file_name, $line, $replacedLine){
+                            $file = explode("\n", rtrim(file_get_contents($file_name)));
+                            $file[$line] = $replacedLine;
+                            $file = implode("\n", $file);
+                            file_put_contents($file_name, $file);
+                          }
+                    }
+                } 
+            }
+        
+        //$pfile.close();
+        
     }
 
 ?>
@@ -63,32 +86,32 @@
             ?>
             " alt="Avatar" class="avatar">
     </div>
-        <?php
-            echo $currentImage;
-        ?>
     <div id=displayUserInfo>
         <table style="width:100%">
+            <?php if(isset($_GET['userEmail']) && isset($_GET['userFname']) && 
+            isset($_GET['userLname']) && isset($_GET['userName'])){ ?>
             <tr>
                 <th>User Name:</th>
-                <td><?php echo $UserName ?></td>
+                <td><?php echo $_GET['userName']; ?></td>
             </tr>
             <tr>
                 <th>Email Address:</th>
-                <td><?php echo $UserEmail ?></td>
+                <td><?php echo $_GET['userEmail']; ?></td>
             </tr>
             <tr>
                 <th>First Name:</th>
-                <td><?php echo $UserFname ?></td>
+                <td><?php echo $_GET['userFname']; ?></td>
             </tr>
             <tr>
                 <th>Last Name:</th>
-                <td><?php echo $UserLname ?></td>
+                <td><?php echo $_GET['userLname']; ?></td>
             </tr>
+            <?php } ?>
             <tr>
                 <th>Password:</th>
                 <td>
-                    <form action ="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="changePasswordform">
-                        <input type="password" name = "password"placeholder="Enter new password">
+                    <form action ="" method="get" name="changePasswordform">
+                        <input type="password" name = "password" id = "password"placeholder="Enter new password">
                         <input type="submit" name="submitBtn" id = "submit" value="Submit">
                     </form>
                 </td>
