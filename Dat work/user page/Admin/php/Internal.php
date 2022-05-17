1c0dccfdfb9ebdbc6f97c5b007246451de38bb13
LyKienDat <s3886334@rmit.edu.vn>
2022-05-17 17:55:10 +0700

    Dat sap xong

---
+++ Dat work/user page/Admin/php/Internal.php
@@ -0,0 +1,11 @@
+<p>This is Internal folder.</p>
+      <?php
+         $images = glob("internal/"."*.{jpeg,jpg,gif,png}",GLOB_BRACE);
+         $publicDescriptions = include 'public/imageDescriptions.txt'; //retrieve the array in the public/imageDescriptions
+         foreach($images as $image) {
+            echo '<img src="'.$image.'" /><br />';
+            $tmpArray = explode("/",$image); //split the source to only get file name + extension
+            echo $publicDescriptions[$tmpArray[1]];//search in imageDescriptions array to find the value of key with same file name
+            ?>
+            <br><br> <!-- skip 2 line -->
+      <?php
